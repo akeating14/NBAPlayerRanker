f14bdf3ffd0c18ee7f609598cc94f1fc04863db7
akeating14 <atk315315@gmail.com>
2016-07-03 18:09:36 -0400

    Added 1 api endpoint that can filter query strings that have lists and limits results to 25 unless given a limit param

--- project/__init__.py
+++ project/__init__.py
@@ -1,16 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_wtf.csrf import CsrfProtect
+from flask_restful import Api
 import os
+
 
 app = Flask(__name__)
 csrf = CsrfProtect(app)
 db = SQLAlchemy(app)
-
+api = Api(app)
 
 app.config.from_object(os.getenv('APP_SETTINGS', 'config.ProductionConfig'))
 
+from project.resources.gamelog import GameLog_Api
+
+api.add_resource(GameLog_Api, '/api/v1/gamelog/')
+
+
+
 from project.player_ranker.views import player_ranker_blueprint
+from project.resources.gamelog import gamelog_api_blueprint
 
 
 app.register_blueprint(player_ranker_blueprint)
+app.register_blueprint(gamelog_api_blueprint)
---
+++ project/resources/gamelog.py
@@ -0,0 +1,83 @@
+from flask import Blueprint
+from flask_restful import Resource, reqparse, fields, marshal_with
+from flask import request
+from project.models import GameLog
+import ast
+
+gamelog_api_blueprint = Blueprint('api', __name__)
+
+resource_fields = {
+                       'id ': fields.String,
+                       'gamelog_id': fields.String,
+                       'season_id': fields.String,
+                       'player_id': fields.String,
+                       'game_id': fields.String,
+                       'game_date': fields.String,
+                       'gap': fields.Integer,
+                       'matchup': fields.String,
+                       'wl': fields.String,
+                       'min_': fields.Integer,
+                       'fgm': fields.Integer,
+                       'fga': fields.Integer,
+                       'fg_pct': fields.Float(4),
+                       'fg3m': fields.Integer,
+                       'fg3a': fields.Integer,
+                       'fg3_pct': fields.Float(4),
+                       'ftm': fields.Integer,
+                       'fta': fields.Integer,
+                       'ft_pct': fields.Float(4),
+                       'oreb': fields.Integer,
+                       'dreb': fields.Integer,
+                       'reb': fields.Integer,
+                       'ast': fields.Integer,
+                       'stl': fields.Integer,
+                       'blk': fields.Integer,
+                       'tov': fields.Integer,
+                       'pf': fields.Integer,
+                       'pts': fields.Integer,
+                       'plus_minus': fields.Integer,
+                       'names': fields.String,
+                       'birthdate': fields.String,
+                       'age': fields.Integer,
+                       'home': fields.String,
+                       'away': fields.String,
+                       'team': fields.String,
+                       'op': fields.String,
+                       'dk_pts': fields.Float(4)}
+
+
+class GameLog_Api(Resource):
+    """
+    TODO:1. Add the arguments to the reg parse
+         2. Add a sorting mechnism
+         3. 2 more endpoints for logs and just by
+         4. Add tests
+         Fun Fact: request.query_string, request.args.get('filter'), request.args
+         are differnt ways to get data from the request.
+    """
+
+    @marshal_with(resource_fields, envelope='resource')
+    def get(self, **kwargs):
+        q = GameLog.query
+        arguments = request.args.get('filter')
+        # parser = reqparse.RequestParser()
+        # args = parser.parse_args()
+        if request.args.get('limit') is None:
+            return_size = 25
+        else:
+            return_size = request.args.get('limit')
+        # arguments return as unicode, so I convert it to a dict
+        if not arguments:
+            args = ast.literal_eval(arguments)
+
+            for attr, value in args.items():
+                """
+                TODO: Apparently checking types is bad so I should try and replace this with
+                duck typing
+                """
+                if isinstance(value, list):
+                    q = q.filter(getattr(GameLog, attr).in_(value))
+                else:
+                    q = q.filter(getattr(GameLog, attr) == value)
+
+        return q.limit(return_size).all()
